16a5cdd9132c4ef2bafc6647149d50ce6d801591
Joanne Snel <sneljoanne@gmail.com>
2021-11-20 20:29:12 +0100

    Update Order model

--- helpers.py
+++ helpers.py
@@ -1,11 +1,12 @@
 import os
+import json
 import requests
 
 
 class RequestHandler:
 
-    def __init__(self, token):
-        self.headers = {'Authorization': f'Bearer {token}'}
+    def __init__(self):
+        self.api_key: str = os.environ.get("API_KEY")
         self.url_trading: str = os.environ.get("TRADING_URL")
         self.url_market: str = os.environ.get("MARKET_URL")
 
@@ -17,16 +18,16 @@ class RequestHandler:
         response = requests.get(self.url_market + endpoint, headers=self.headers)
         return response.json()
 
-    def put_data(self, endpoint: str):
-        response = requests.put(self.url_trading + endpoint, headers=self.headers)
-        return response.json()
-
     def post_data(self, endpoint: str, data):
         response = requests.post(self.url_trading + endpoint,
-                                 data,
+                                 json.dumps(data),
                                  headers=self.headers)
         return response.json()
 
     def delete_data(self, endpoint: str):
         response = requests.delete(self.url_trading + endpoint, headers=self.headers)
         return response.json()
+
+    @property
+    def headers(self):
+        return {"Authorization": f"Bearer {self.api_key}"}
